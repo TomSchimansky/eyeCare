38a27de6bf36af719f7b2761c1b18b5543c825b8
Tom Schimansky <tomschimansky@gmail.com>
2024-01-12 18:10:46 +0100

    add readme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# eyeCare
+
+---
+
+Test
--- model_training/main_training.py
+++ model_training/main_training.py
@@ -1,9 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator
-
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.callbacks import Callback
+import os
 
 from model_evaluation.real_accuracy_test import test_accuracy
 
