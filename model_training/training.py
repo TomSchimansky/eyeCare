28c1068e721dba6dea4ead25de3abcab96ca92f6
Tom Schimansky <tomschimansky@gmail.com>
2024-01-12 21:23:11 +0100

    enhance training

--- model_training/training.py
+++ model_training/training.py
@@ -8,32 +8,33 @@ from keras.optimizers import Adam
 IMAGE_SIZE = 32  # px
 BATCH_SIZE = 16
 
-datagenerator = ImageDataGenerator(rotation_range=30,
-                                   width_shift_range=0.1,
-                                   height_shift_range=0.1,
-                                   brightness_range=(0.9, 1.1),
-                                   shear_range=0.1,
-                                   zoom_range=0.1,
-                                   rescale=1/255,
-                                   fill_mode='nearest',
-                                   validation_split=0)
+train_datagenerator = ImageDataGenerator(rotation_range=30,
+                                         width_shift_range=0.1,
+                                         height_shift_range=0.1,
+                                         brightness_range=(0.9, 1.1),
+                                         shear_range=0.1,
+                                         zoom_range=0.1,
+                                         rescale=1 / 255,
+                                         fill_mode='nearest')
 
-train_data = datagenerator.flow_from_directory("mrlEyes_open_closed/train",
-                                               classes=["open", "closed"],
-                                               target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                               batch_size=BATCH_SIZE,
-                                               color_mode="grayscale",
-                                               subset="training",
-                                               class_mode="binary",
-                                               shuffle=True)
+valid_datagenerator = ImageDataGenerator(rescale=1 / 255)
 
-validation_data = datagenerator.flow_from_directory("mrlEyes_open_closed/valid",
-                                                    classes=["open", "closed"],
-                                                    target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                                    batch_size=BATCH_SIZE,
-                                                    color_mode="grayscale",
-                                                    subset="training",
-                                                    class_mode="binary")
+train_data = train_datagenerator.flow_from_directory("mrlEyes_open_closed/train",
+                                                     classes=["open", "closed"],
+                                                     target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                                     batch_size=BATCH_SIZE,
+                                                     color_mode="grayscale",
+                                                     subset="training",
+                                                     class_mode="binary",
+                                                     shuffle=True)
+
+validation_data = valid_datagenerator.flow_from_directory("mrlEyes_open_closed/valid",
+                                                          classes=["open", "closed"],
+                                                          target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                                          batch_size=BATCH_SIZE,
+                                                          color_mode="grayscale",
+                                                          subset="training",
+                                                          class_mode="binary")
 
 cp_callback = ModelCheckpoint(filepath=f"checkpoints/model_{IMAGE_SIZE}_{{epoch:03d}}-{{val_accuracy:.3f}}.hdf5",
                               monitor='val_accuracy',
@@ -42,11 +43,15 @@ cp_callback = ModelCheckpoint(filepath=f"checkpoints/model_{IMAGE_SIZE}_{{epoch:
                               mode='max')
 
 model = Sequential()
-model.add(Conv2D(64, (6, 6), input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1)))
+model.add(Conv2D(64, (5, 5), input_shape=(IMAGE_SIZE, IMAGE_SIZE, 1)))
 model.add(Activation('softplus'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(128, (4, 4)))
+model.add(Activation('softplus'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
 model.add(Activation('softplus'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -67,8 +72,8 @@ model.compile(loss='binary_crossentropy',
 
 model.summary()
 model.fit(train_data,
-          steps_per_epoch=train_data.samples // BATCH_SIZE,
-          epochs=50,
           validation_data=validation_data,
+          epochs=50,
+          steps_per_epoch=train_data.samples // BATCH_SIZE,
           validation_steps=validation_data.samples // BATCH_SIZE,
           callbacks=[cp_callback])
